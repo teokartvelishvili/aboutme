232ef914b171135f7c39cfa094d491f7db649bc7
teokartvelishvili <qartvelishviliteona@gmail.com>
2024-05-09 10:54:12 -0800

    contactPage

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,16 +9,23 @@ import logoDark from "../../Assets/teoLogoDark.png";
 import ToggleSwitch from "../Toggle/Toggle";
 import lemon from '../../Assets/Property 1=orange, Property 2=on.png';
 import orange from '../../Assets/Property 1=orange, Property 2=off.png';
+import engFlag from '../../Assets/engFlag.png';
+import geoFlag from '../../Assets/geoFlag.png';
 
 
 const Header = () => {
     const { theme, toggleTheme } = useContext(ThemeContext); // Access ThemeContext
     const [isChecked, setIsChecked] = useState(theme === "dark");
+    // const { language, setLanguage } = useContext(LanguageContext);
 
     const handleChange = () => {
         setIsChecked(!isChecked);
         toggleTheme(); // Toggle theme
     };
+    // const handleLangClick = () => {
+    //     const newLanguage = language === "ge" ? "en" : "ge";
+    //     setLanguage(newLanguage);
+    //   };
 
     return (
         <div className={`headerMainDiv ${theme}`}>
@@ -39,9 +46,17 @@ const Header = () => {
                 onChange={handleChange} 
                 image={lemon} 
                 image2={orange}
-          
+            /></div>
+
+        {/* <div className="LengToggle">
+            <ToggleSwitch 
+                checked={isChecked} 
+                onChange={handleLangClick} 
+                image={engFlag} 
+                image2={geoFlag}
             />
-        </div>
+        </div> */}
+
             <div className="navbar">
                 <Navbar/>
             </div>
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,13 +1,137 @@
-import React from "react";
-import "./Contact.css"
 
+
+import React, { useState } from "react";
+import "./Contact.css";
+// import { TEXTS } from "../../Hooks/Languages";
+// import { LanguageContext } from "../../Hooks/LanguageContext";
+import { ThemeContext } from "../../Hooks/ThemeContecs";
+import git from '../../Assets/GitHub.webp';
+import linkedin from '../../Assets/linkedin.webp';
+import fb from '../../Assets/facebook.webp';
+import inst from '../../Assets/instagram.webp';
+import mail from '../../Assets/mail.webp';
+import call from '../../Assets/calls.webp'
+import { useContext } from "react";
 
 const Contact = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext); 
+  // const { language, setLanguage } = useContext(LanguageContext);
+  // State variables to store form data
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create FormData object to store form data
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("subject", subject);
+    formData.append("message", message);
+
+    try {
+      // Send form data to Formspree endpoint
+      const response = await fetch("https://formspree.io/f/xvoeyody", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      // Check if form submission was successful
+      if (response.ok) {
+        setSubmitted(true);
+        // Reset form fields
+        setName("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
   return (
-    <div className="MainCont">
-      <h1>Contact</h1>
-      
+    <div className={`contact ${theme}`}>
+      <div className="section1">
+        {/* <h1>{TEXTS[language].contactMe}</h1> */}
+        <h1>contact me</h1>
+
+        {submitted ? (
+          <p>message Sent, thank you! </p>
+        ) : (
+          <form onSubmit={handleSubmit} className="contact-form">
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Your Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                placeholder="Your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            {/* <div className="form-group">
+              <input
+                type="text"
+                placeholder="Subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                required
+              />
+            </div> */}
+            <div className="form-group">
+              <textarea
+                placeholder="Your Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              ></textarea>
+            </div>
+            <button type="submit">Send</button>
+          </form>
+        )}
+      </div>
+
+      <div className="socIcons">
+        <a href="https://github.com/teokartvelishvili?tab=repositories" target="blank">
+          {" "}
+          <img alt="icon" src={git} />
+        </a>
+        <a href="https://www.linkedin.com/in/teona-qartvelishvili-8a4005257/" target="blank">
+          {" "}
+          <img alt="icon" src={linkedin} />
+        </a>
+        <a href="https://www.instagram.com/teo.kartvel?igsh=dnUzODMxcnZzN3p5&utm_source=qr" target="blank">
+          <img alt="icon" src={inst} />
+        </a>
+        <a href="https://www.facebook.com/teo.qartvel" target="blank">
+          <img alt="icon" src={fb} />
+        </a>
+        <a href="mailto:qartvelishviliteona@gmail.com">
+          <img alt="icon" src={mail} />
+        </a>
+        <a href="tel:+995551999055">
+          <img alt="icon" src={call} />
+        </a>
+      </div>
     </div>
   );
 };
-export default Contact
+
+export default Contact;
