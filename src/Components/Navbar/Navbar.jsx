3216f4069f343ecdfd5f5df78200850808e5d4a2
teokartvelishvili <qartvelishviliteona@gmail.com>
2024-05-12 13:16:35 -0800

    burger-menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useContext } from "react";
 import { ThemeContext } from "../../Hooks/ThemeContecs";
-import { Link } from "react-router-dom"
-import "./Navbar.css"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
         <div className={`NavCont ${theme}`}>
-        <ul className={`UlCont ${theme}`}>
-            <li><Link to="/">Home</Link> </li>
-            <li><Link to="/Projects"> My Projects</Link> </li>
-            <li><Link to="/About">About Me</Link> </li>
-            <li><Link to="/Contact">Contact</Link> </li>
-        </ul>
+            <div className="hamburger-menu" onClick={toggleMenu}>
+                <div className={`burger ${isOpen ? 'open' : ''}`}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+            </div>
+            <ul className={`UlCont ${isOpen ? "open" : ""}`}>
+                <li><Link to="/">Home</Link> </li>
+                <li><Link to="/Projects">My Projects</Link> </li>
+                <li><Link to="/About">About Me</Link> </li>
+                <li><Link to="/Contact">Contact</Link> </li>
+            </ul>
         </div>
-        
-    )
-}
-export default Navbar
+    );
+};
+
+export default Navbar;
